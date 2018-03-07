e5943dae3ccba7872c38d7d35257bd6527e5bf54
Arthur Cheung <arthur.cheung@accenture.com>
2018-03-07 13:42:21 +1100

    Removed '/' from 'home' route.

--- src/app/landing-page/landing-page-routing.module.ts
+++ src/app/landing-page/landing-page-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page.component';
 
 const routes: Routes = [
-  {path: '/home', component: LandingPageComponent}
+  {path: 'home', component: LandingPageComponent}
 ];
 
 @NgModule({
