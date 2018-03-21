e355112e8e5b94da2bb5a5c36c05360dabc9c210
Arthur Cheung <arthur.cheung@accenture.com>
2018-03-21 12:46:19 +1100

    Added TriviaService to core module providers.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { RouteLinksComponent } from './route-links/route-links.component';
+import { TriviaService } from './services/trivia.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    HttpClientModule
   ],
   declarations: [RouteLinksComponent],
   exports: [
     RouteLinksComponent
+  ],
+  providers: [
+    TriviaService
   ]
 })
 export class CoreModule { }
