1d7e3e51b5c8093b8251821d08a3372d1fcf3beb
Arthur Cheung <arthur.cheung@accenture.com>
2018-06-07 16:49:41 +1000

    Added explanation for core-routing in comments.

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,3 +1,7 @@
+// This file is here so that the dynamic route links can work
+// There's not actually any routes here, but to access the routes to generate links at the top of the page
+// We need routing module here.
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
